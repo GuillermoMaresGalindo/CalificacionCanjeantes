fef943cf675383221727750f8c4468be07780fe7
GuillermoMaresGalindo <106297487+GuillermoMaresGalindo@users.noreply.github.com>
2022-08-14 15:12:08 -0700

    Update prueba.py
    a

--- prueba.py
+++ prueba.py
@@ -1,43 +1,23 @@
 import streamlit as st
+import streamlit_authenticator as stauth
 # streamlit_app.py
+names = ['John Smith','Rebecca Briggs']
+usernames = ['jsmith','rbriggs']
+passwords = ['123','456']
+hashed_passwords = stauth.hasher(passwords).generate()
+authenticator = stauth.authenticate(names,usernames,hashed_passwords,'cookie_name', 'signature_key',cookie_expiry_days=30)
+name, authentication_status = authenticator.login('Login','sidebar')
 
-import streamlit as st
+if authentication_status:
+ st.write("Welcome *%s*" % (name))
+ x = st.slider('Select a value')
+ st.write(x, 'squared is', x * x)
+ st.button("Click me")
+elif authentication_status == False:
+ st.error("Username/password is incorrect")
+elif authentication_status == None:
+ st.warning("Please enter your username and password")
 
-def check_password():
-    """Returns `True` if the user had the correct password."""
 
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
 
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        # Password correct.
-        return True
-        
-def new_func():
-    x = st.slider('Select a value')
-    return x
-
-if check_password():
-    st.write("Here goes your normal Streamlit app...")
-    x = new_func()
-    st.write(x, 'squared is', x * x)
-    st.button("Click me")
 
