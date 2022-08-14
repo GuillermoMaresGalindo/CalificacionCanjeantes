939b20bddbe22916a4eba4e6087bb382e72c19f0
GuillermoMaresGalindo <106297487+GuillermoMaresGalindo@users.noreply.github.com>
2022-08-14 15:24:29 -0700

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,18 +1,40 @@
 import streamlit as st
-import streamlit_authenticator as stauth
-# streamlit_app.py
-names = ['John Smith','Rebecca Briggs']
-usernames = ['jsmith','rbriggs']
-passwords = ['123','456']
-hashed_passwords = stauth.hasher(passwords).generate()
-authenticator = stauth.authenticate(names,usernames,hashed_passwords,'cookie_name', 'signature_key',cookie_expiry_days=30)
-name, authentication_status = authenticator.login('Login','sidebar')
 
-if authentication_status:
- st.write("Welcome *%s*" % (name))
- x = st.slider('Select a value')
- st.write(x, 'squared is', x * x)
-elif authentication_status == False:
- st.error("Username/password is incorrect")
-elif authentication_status == None:
- st.warning("Please enter your username and password")
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if (
+            st.session_state["username"] in st.secrets["passwords"]
+            and st.session_state["password"]
+            == st.secrets["passwords"][st.session_state["username"]]
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store username + password
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show inputs for username + password.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 User not known or password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+    x = st.slider('Select a value')
+    st.write(x, 'squared is', x * x)
